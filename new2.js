d19eed20eb13643f52724e4b30d024084529afe1
DariaTkacheva <90569700+DariaTkacheva@users.noreply.github.com>
2021-10-12 17:45:21 +0300

    Add files via upload

---
+++ new2.js
@@ -0,0 +1,31 @@
+
+var btn = document.querySelector('#btn');
+var out = document.querySelector('#out');
+var cena = document.querySelector('#cena');
+var kol = document.querySelector('#kol');
+var c=cena.value;
+var k=kol.value;
+
+
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  console.log("DOM fully loaded and parsed");
+btn.onclick = function()
+ { const r = /-+?|\b0[0-9]+/;
+  const r1 = /\D+/;
+  const r2 = /^$/;
+  var pro = parseInt(cena.value) *parseInt(kol.value);
+    if((r.test(cena.value) ||r.test(kol.value))===true ) {
+      alert("Ошибка: Число меньше либо равно нулю")
+
+}
+ else if((r1.test(cena.value) ||r1.test(kol.value))===true ) {
+  alert(" Ошибка: Вы ввели букву, а не число!")
+
+}
+else if((r2.test(cena.value) ||r2.test(kol.value))===true ) {
+  alert(" Ошибка: Введите цифру в соответствующее поле!")
+
+}
+    else {out.innerHTML = pro;ob();} }
+ });
